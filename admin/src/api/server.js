b1134f1c706aa93a13ae165dec145338c50635f6
glossycodedev <134337694+glossycodedev@users.noreply.github.com>
2024-06-20 21:46:22 +0300

    server api

--- admin/src/api/server.js
+++ admin/src/api/server.js
@@ -1,7 +1,5 @@
-export const backend_url = 'http://localhost:5000/uploads';
-// export const backend_url_img = 'http://localhost:5000';
 
-// export const server = 'https://delos-ops.com/api/v2';
+export const backend_url_img = 'http://localhost:5000';
 
-// export const backend_url = 'https://delos-ops.com/api/';
-export const backend_url_img = 'https://54.198.12.163';
+
+// export const backend_url_img = 'https://54.198.12.163';
