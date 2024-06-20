a35f85068086b614eb8529e9558c111f2594adf1
glossycodedev <134337694+glossycodedev@users.noreply.github.com>
2024-06-20 21:39:14 +0300

    design update

--- admin/src/api/api.js
+++ admin/src/api/api.js
@@ -9,7 +9,7 @@
 
 import axios from 'axios';
 const local = 'http://localhost:5000';
-const REACT_APP_API_URL = 'https://54.198.12.163';
+// const REACT_APP_API_URL = 'https://54.198.12.163';
 const production = '';
 const api = axios.create({
   baseURL: `${local}/api`,
