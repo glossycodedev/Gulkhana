d500460f3944d063c3c20dff0432372a18a9e6cb
glossycodedev <134337694+glossycodedev@users.noreply.github.com>
2024-06-20 23:09:55 +0300

    header logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -122,6 +122,8 @@ const Header = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const headerLogo = '../../../images/logo.png';
+
   return (
     <div className="w-full bg-white">
       <div className="header-top bg-[#FEEAF1] md-lg:hidden">
@@ -197,13 +199,19 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="w-full bg-white shadow-sm z-50 " >
+      <div className="w-full bg-white shadow-sm z-50 ">
         <div className="w-[85%] lg:w-[90%] mx-auto">
           <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
             <div className="md-lg:w-full w-3/12 md-lg:pt-4">
               <div className="flex justify-between items-center">
                 <Link to="/">
-                  <img src="http://localhost:3001/images/logo.png" alt="" />
+                <img
+                // src="http://localhost:3001/images/logo.png"
+                src={headerLogo}
+                className="w-[190px] h-[80px]"
+                alt=""
+              />
+                  {/* <img src="http://localhost:3001/images/logo.png" alt="" /> */}
                 </Link>
                 <div
                   className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden"
@@ -276,11 +284,10 @@ const Header = () => {
                       {t('home.contact_us')}
                     </Link>
                   </li>
-                  
                 </ul>
 
                 <div className="flex md-lg:hidden justify-center items-center gap-5">
-                 <div className="flex justify-center gap-5">
+                  <div className="flex justify-center gap-5">
                     <span
                       onClick={() => setSearchActive(!searchActive)}
                       className="text-3xl cursor-pointer text-[#B65278]"
@@ -290,7 +297,6 @@ const Header = () => {
                   </div>
                 </div>
                 <div className="flex md-lg:hidden justify-center items-center gap-5">
-                
                   <div className="flex justify-center gap-5">
                     <div
                       onClick={() =>
@@ -348,7 +354,12 @@ const Header = () => {
         >
           <div className="flex justify-start flex-col gap-6">
             <Link to="/">
-              <img src="http://localhost:3001/images/logo.png" alt="" />
+              <img
+                // src="http://localhost:3001/images/logo.png"
+                src={headerLogo}
+                className="w-[190px] h-[70px]"
+                alt=""
+              />
             </Link>
             <div className="flex justify-start items-center gap-10">
               <div className="flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute ">
@@ -399,7 +410,7 @@ const Header = () => {
             <ul className="flex flex-col justify-start items-start text-sm rtl:text-lg font-bold uppercase">
               <li>
                 <Link
-                 to="/"
+                  to="/"
                   className={`py-2 block ${
                     pathname === '/' ? 'text-[#B65278]' : 'text-slate-600'
                   } `}
@@ -448,17 +459,17 @@ const Header = () => {
                 </Link>
               </li>
               <div className="flex  justify-center items-center gap-5">
-                 <div className="flex justify-center gap-5">
-                 <span
-                      onClick={() => setSearchActive(!searchActive)}
-                      className="text-3xl cursor-pointer text-[#B65278]"
-                    >
-                      <CiSearch />
-                    </span>
-                  </div>
+                <div className="flex justify-center gap-5">
+                  <span
+                    onClick={() => setSearchActive(!searchActive)}
+                    className="text-3xl cursor-pointer text-[#B65278]"
+                  >
+                    <CiSearch />
+                  </span>
                 </div>
+              </div>
             </ul>
-            
+
             <div className="flex justify-start items-center gap-4 text-black">
               <a href="#">
                 <FaFacebookF />
@@ -586,8 +597,6 @@ const Header = () => {
                     </button>
                   </div>
                 </div>
-
-                
               </div>
             </div>
           )}
